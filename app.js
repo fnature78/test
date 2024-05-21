938dffe1f9b643babc1c96056fb7fac7b5b4bcf1
fnature78 <ntr.fabien@gmail.com>
2024-05-21 13:27:10 +0200

    test modification contenu site

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Load the http module to create an http server.
 var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) { response.writeHead(200, {"Content-Type": "text/plain"}); response.end("Hello World");
+var server = http.createServer(function (request, response) { response.writeHead(200, {"Content-Type": "text/plain"}); response.end("Hello World by Fabien test ci");
 });
 // Listen on port 8000, IP defaults to "0.0.0.0"
 server.listen(8000);
